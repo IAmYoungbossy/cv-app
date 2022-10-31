21b7df7adca94c6ca58e44a67fba918a263e1bd6
Letam Bossman Barinua <letambarinua@gmail.com>
2022-10-31 11:54:40 +0100

    Add Footer class component to Fooder directory and exported to App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,10 @@
+import Footer from '../Footer/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <small>
+          Copyright &#169; 2022 IAmYoungbossy{" "}
+          <a href="https://github.com/IAmYoungbossy/cv-app">
+            <i className="fa fa-github"></i>
+          </a>
+        </small>
+      </footer>
+    );
+  }
+}
