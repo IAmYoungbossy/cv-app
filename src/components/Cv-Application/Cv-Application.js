a8f8feb1dcc73d14d1a4d02462b625cd87a9f1cf
Letam Bossman Barinua <letambarinua@gmail.com>
2022-11-09 13:27:40 +0100

    Add name header to Cv file

--- src/components/Cv-Application/Cv-Application.js
+++ src/components/Cv-Application/Cv-Application.js
@@ -1,9 +1,21 @@
 import { Component } from "react";
+import "./Cv-Application.css";
 
 export default class CvApp extends Component {
   render() {
     return (
-      <h1>CV Application</h1>
-    )
+      <div className="cv">
+        <div className="cv-header">
+          <div className="title">
+            <p className="name-title">Mr.</p>
+            <h1>Letam Bossman Barinua</h1>
+          </div>
+        </div>
+        <div className="cv-body">
+          <div className="other-fields"></div>
+          <div className="personal-details"></div>
+        </div>
+      </div>
+    );
   }
 }
