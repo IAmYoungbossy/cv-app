85260c069a03f22a488ea1b267937345d8d3bf0a
Letam Bossman Barinua <letambarinua@gmail.com>
2022-11-16 11:31:27 +0100

    Add previewCV.js file to Form component for previewing CV

---
+++ src/components/Form/previewCv.js
@@ -0,0 +1,121 @@
+import uniqid from "uniqid";
+
+const previewCV = [
+  [
+    {
+      job: "Mr.",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus urna neque viverra justo nec ultrices dui sapien. Aliquam eleifend mi in nulla. Facilisi cras fermentum odio eu feugiat pretium nibh ipsum.",
+      fname: "Letam",
+      lname: "Barinua",
+      phone: "07012345678",
+      email: "letamexample@example.com",
+      photo: "",
+      province: "Rivers State",
+      uniqueID: uniqid(),
+    },
+  ],
+  [
+    {
+      uniqueID: uniqid(),
+      position: "Internship Trainee",
+      company: "Nigeria Stored Product Research Institute",
+      city: "Port Harcourt, Nigeria",
+      from: "2017",
+      to: "2017",
+    },
+    {
+      uniqueID: uniqid(),
+      position: "P2P Bitcoin Trader",
+      company: "Remitano",
+      city: "Port Harcourt, Nigeria",
+      from: "2017",
+      to: "2018",
+    },
+    {
+      uniqueID: uniqid(),
+      position: "Bolt Driver",
+      company: "Bolt Nigeria",
+      city: "Port Harcourt, Nigeria",
+      from: "2019",
+      to: "2020",
+    },
+    {
+      uniqueID: uniqid(),
+      position: "IT Instructor",
+      company: "Nigeria Youth Service Corps",
+      city: "Port Harcourt, Nigeria",
+      from: "2020",
+      to: "2021",
+    },
+    {
+      uniqueID: uniqid(),
+      position: "Discord Moderator",
+      company: "Forex Tips 101, Canada",
+      city: "Port Harcourt, Nigeria",
+      from: "2021",
+      to: "2021",
+    },
+  ],
+  [
+    {
+      uniqueID: uniqid(),
+      university: "Rivers State University",
+      degree: "Agricultural and Environmental Engineering",
+      course: "B.Tech",
+      from: "2013",
+      city: "Port Harcourt, Nigeria",
+      to: "2018",
+    },
+    {
+      uniqueID: uniqid(),
+      university: "The Odin Project",
+      degree: "Software Developer",
+      course: "Full Stack Web Developer",
+      from: "2022",
+      city: "New York",
+      to: "2023",
+    },
+  ],
+  [
+    {
+      uniqueID: uniqid(),
+      skillListArr: [
+        {
+          uniqueID: uniqid(),
+          skillName: "HTML",
+        },
+        {
+          uniqueID: uniqid(),
+          skillName: "CSS",
+        },
+        {
+          uniqueID: uniqid(),
+          skillName: "JavaScript",
+        },
+      ],
+      skillCategory: "Web Dev Basics",
+    },
+    {
+      uniqueID: uniqid(),
+      skillListArr: [
+        {
+          uniqueID: uniqid(),
+          skillName: "MongoDB",
+        },
+        {
+          uniqueID: uniqid(),
+          skillName: "ExpressJS",
+        },
+        {
+          uniqueID: uniqid(),
+          skillName: "ReactJS",
+        },
+        {
+          uniqueID: uniqid(),
+          skillName: "NodeJS",
+        },
+      ],
+      skillCategory: "Frameworks",
+    },
+  ],
+];
